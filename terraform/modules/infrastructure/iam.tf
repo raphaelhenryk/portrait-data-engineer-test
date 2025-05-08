ec98882789c4090f5dc8b96bfcc8bd056502926b
raphaelhenryk <rhssraphael@gmail.com>
2025-05-07 23:31:45 -0300

    Last commit

---
+++ terraform/modules/infrastructure/iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_user" "s3_uploader" {
+  name = "${var.project_name}-airflow-s3-uploader"
+}
+
+resource "aws_iam_user_policy" "s3_policy" {
+  name = "${var.project_name}-s3-access"
+  user = aws_iam_user.s3_uploader.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["s3:*"],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "s3_user_key" {
+  user = aws_iam_user.s3_uploader.name
+}
+
+output "aws_access_key_id" {
+  value     = aws_iam_access_key.s3_user_key.id
+  sensitive = true
+}
+
+output "aws_secret_access_key" {
+  value     = aws_iam_access_key.s3_user_key.secret
+  sensitive = true
+}
